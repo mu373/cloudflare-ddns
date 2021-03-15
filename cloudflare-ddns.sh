36937789e1329ae72c7da938307e355ea256f8b8
Minami Ueda <minami.ueda@gmail.com>
2021-03-15 12:39:04 +0900

    Keep log only when keep_log is true

--- cloudflare-ddns.sh
+++ cloudflare-ddns.sh
@@ -7,6 +7,7 @@ auth_email="mail@example.com"            # The email used to login 'https://dash
 auth_key="xxxxx"   # Top right corner, "My profile" > "Global API Key"
 zone_identifier="xxxxx" # Can be found in the "Overview" tab of your domain
 record_name="foo.example.com"                     # Which record you want to be synced
+keep_log=true
 
 # Fetch Global IP address
 ip=$(curl -s inet-ip.info)
@@ -45,8 +46,10 @@ update=$(curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$zone_identi
 	--data "{\"type\":\"A\",\"name\":\"$record_name\",\"content\":\"$ip\",\"ttl\":1}")
 
 # Log IP address if it has changed
-currenttime=$(date '+%Y-%m-%dT%H:%M%z')
-echo "$currenttime, $ip" >> ip-address.txt
+if "${keep_log}"; then
+	currenttime=$(date '+%Y-%m-%dT%H:%M%z')	
+	echo "$currenttime, $ip" >> ip-address.txt
+fi
 
 # Result
 success=$(echo $update | jq -r '.success')
